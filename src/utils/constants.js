b4828fb8a86103a0351364881d9fbd112f53a10a
Matias M <matiasmendoza1994@gmail.com>
2024-03-25 23:11:33 -0300

    feature/ecommerce: Configuro las constantes para acceder al bff desde localhost:8080.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,6 @@ export const AUTHOR = {
   name: 'Matias',
   lastname: 'Mendoza',
 };
-export const ITEM_SEARCH_ENDPOINT = 'http://localhost:3000/api/items';
-export const ITEM_DETAIL_ENDPOINT = 'http://localhost:3000/api/items/:id';
+export const ITEM_SEARCH_ENDPOINT = 'http://localhost:8080/api/items';
+export const ITEM_DETAIL_ENDPOINT = 'http://localhost:8080/api/items/:id';
 export const ITEM_ID_KEY = ':id';
